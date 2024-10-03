a14cc79a1ac2ed101ac86ad62268330f0342d971
phucngdev <98637507+phucngdev@users.noreply.github.com>
2024-10-03 21:39:02 +0700

    test

--- server/src/app.js
+++ server/src/app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
     // origin: "http://localhost:5173",
-    origin: "https://teelab-v1.vercel.app/",
+    origin: "*",
     credentials: true,
   })
 );
