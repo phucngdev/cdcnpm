f7ff5334eaceb3d68178b0aea946c6652675b990
phucngdev <98637507+phucngdev@users.noreply.github.com>
2024-10-03 21:43:00 +0700

    test

--- server/src/app.js
+++ server/src/app.js
@@ -16,13 +16,13 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  cors({
-    // origin: "http://localhost:5173",
-    origin: "*",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+app.use(cors());
 app.use(morgan("common"));
 app.use(express.json());
 app.use(cookieParser());
